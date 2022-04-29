c3a6f7beb8871470819a7ecf6a10fc2aa7b4d31c
chrisephoto <chrisephotography@gmail.com>
2022-04-29 08:22:16 -0400

    Initial release of database.js

---
+++ database.js
@@ -0,0 +1,14 @@
+database = [
+  {
+    title: "Sailor Moon R the Movie: Promise of the Rose",
+    image: "https://img1.ak.crunchyroll.com/i/spire1/a0f45608c4e4f48c89936055c0dbdb601651150200_main.jpg"
+  },
+  {
+    title: "Attack on Titan: The Final Season",
+    image: "https://s1.zerochan.net/Attack.on.Titan.600.1417657.jpg"
+  },
+  {
+    title: "My Hero Academia",
+    image: "https://s1.zerochan.net/Boku.no.Hero.Academia.600.2139574.jpg"
+  }
+];
