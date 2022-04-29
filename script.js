0faa77cd8ecb6446f2e6fa2f2a736800dc83037d
chrisephoto <chrisephotography@gmail.com>
2022-04-29 08:32:27 -0400

    Added function to load data from database.js

--- script.js
+++ script.js
@@ -1,62 +1,82 @@
-window.onscroll = function updateTheme() {
-	m = document.getElementById("top").getBoundingClientRect().top;
-	console.log(m);
-	if (m < -32) {
-		document.getElementById("top-button").classList.add("visible")
-	}
-	else {
-		document.getElementById("top-button").classList.remove("visible")
-	}
+window.addEventListener("load", () => {
+  loadData();
+  loadTheme();
+});
+
+window.addEventListener("scroll", () => {
+  scrollEffects();
+});
+
+function loadData() {
+  for (let i = 0; i < database.length; i++) {
+    const content = document.getElementById("content");
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+    const figcaption = document.createElement("figcaption");
+    const text = document.createTextNode(database[i].title);
+    const div = document.createElement("div");
+
+    content.appendChild(figure);
+    figure.className = "card";
+    figure.appendChild(img);
+    img.src = database[i].image;
+    figure.appendChild(figcaption);
+    figcaption.appendChild(text);
+  }
 }
 
-window.onload = function updateTheme() {
-	z = localStorage.getItem("theme");
-	if (z) {
-		document.querySelector("body").className = z;
-		document.querySelector("input[type=radio][name=theme-radio][value=" + z + "]").checked = true;
-	}
-	else {
-		localStorage.setItem("theme", "theme-light"); //set default theme
-		updateTheme();
-	}
-	y = localStorage.getItem("accent");
-	if (y) {
-		document.querySelector(":root").style.setProperty("--accent-color", "#" + y);
-		document.querySelector("input[type=radio][name=accent-radio][value=" + y + "]").checked = true;
-	}
-	else {
-		localStorage.setItem("accent", "dd191d"); //set default accent
-		updateTheme();
-	}
+function loadTheme() {
+  a = localStorage.getItem("theme");
+  if (a) {
+    document.querySelector("body").className = a;
+    document.querySelector("input[type=radio][name=theme-radio][value=" + a + "]").checked = true;
+  } else {
+    localStorage.setItem("theme", "theme-light"); //set default theme
+    loadTheme();
+  }
+  b = localStorage.getItem("accent");
+  if (b) {
+    document.querySelector(":root").style.setProperty("--accent-color", "#" + b);
+    document.querySelector("input[type=radio][name=accent-radio][value=" + b + "]").checked = true;
+  } else {
+    localStorage.setItem("accent", "dd191d"); //set default accent
+    loadTheme();
+  }
+}
+
+function scrollEffects() {
+  a = document.getElementById("top").getBoundingClientRect().top;
+  if (a < -32) {
+    document.getElementById("top-button").classList.add("visible")
+  } else {
+    document.getElementById("top-button").classList.remove("visible")
+  }
 }
 
 a = document.querySelectorAll("input[type=radio][name=theme-radio]");
 Array.prototype.forEach.call(a, function(radio) {
-   radio.addEventListener("change", function() {
-		 document.querySelector("body").className = radio.value;
-		 localStorage.setItem("theme", radio.value);
-	 });
+  radio.addEventListener("change", function() {
+    localStorage.setItem("theme", radio.value);
+	  loadTheme();
+  });
 });
 
 b = document.querySelectorAll("input[type=radio][name=accent-radio]");
 Array.prototype.forEach.call(b, function(radio) {
-   radio.addEventListener("change", function() {
-		 document.querySelector(":root").style.setProperty("--accent-color", "#" + radio.value);//
-		 localStorage.setItem("accent", radio.value);
-	 });
+  radio.addEventListener("change", function() {
+    localStorage.setItem("accent", radio.value);
+    loadTheme();
+  });
 });
 
-c = document.getElementById("settings-button");
-c.addEventListener("click", function () {
+document.getElementById("settings-button").addEventListener("click", function() {
   document.getElementById("settings").open = true;
-}, false);
+}
 
 d = document.getElementById("filter");
 d.addEventListener("input", function filter() {
   var filter, ul, li, a, i, txtValue;
-  console.log(d);
-	filter = d.value.toUpperCase();
-	console.log(filter);
+  filter = d.value.toUpperCase();
   figures = document.querySelectorAll("figure");
 
   // Loop through all list items, and hide those who don't match the search query
@@ -65,8 +85,7 @@ d.addEventListener("input", function filter() {
     txtValue = figcaption.textContent || figcaption.innerText;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
       figures[i].style.display = "";
-    }
-		else {
+    } else {
       figures[i].style.display = "none";
     }
   }
